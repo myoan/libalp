8dedd7d976a4712a455dbb792445620a5551fbdb
myoan <motoki.yoan@gmail.com>
2022-07-22 19:24:19 +0900

    Rename script and fix sample

--- example/main.go
+++ example/main.go
@@ -7,18 +7,16 @@ import (
 )
 
 func main() {
-	p, err := libalp.NewAlpProfiler("./alp.log")
+	p, err := libalp.NewAlpProfiler("/tmp/alp.log")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	err = p.Run(libalp.AlpOption{
-		File:           "/Users/yoan/dev/github.com/myoan/myalp/access.log",
-		Reverse:        true,
-		Limit:          10000,
-		MatchingGroups: "/api/estate/\\d+,/api/estate/req_doc/\\d+,/api/chair/.+,/api/recommended_estate/.+",
-		SortType:       "p99",
-		Percentiles:    []int{50, 90, 99},
+		File:        "/var/log/nginx/access.log",
+		Reverse:     true,
+		SortType:    "p99",
+		Percentiles: []int{50, 90, 99},
 	})
 
 	if err != nil {
